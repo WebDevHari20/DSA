80c4ce80a68db873ad5a475afd59ec8f0ddedadf
Hari Bahadur Narzary <harinarzary22@gmail.com>
2026-03-11 08:32:12 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (54.65%) - LeetSync

---
+++ 861-flipping-an-image/flipping-an-image.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def flipAndInvertImage(self, image):
+        """
+        :type image: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        for row in image:
+            row.reverse()
+            for i in range(len(image)):
+                row[i] = 1-row[i]
+        return image
+        
