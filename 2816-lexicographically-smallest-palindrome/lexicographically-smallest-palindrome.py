fe7749fc2f6a55fa555520ff6810a9dd51e2b213
Hari Bahadur Narzary <harinarzary22@gmail.com>
2026-03-23 08:36:14 +0530

    Time: 108 ms (20.00%) | Memory: 12.6 MB (49.47%) - LeetSync

---
+++ 2816-lexicographically-smallest-palindrome/lexicographically-smallest-palindrome.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def makeSmallestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        s = list(s)
+        i, j = 0, len(s)-1
+        while i<j:
+            if s[i] != s[j]:
+                smallest = min(s[i], s[j])
+                s[i] = smallest
+                s[j] = smallest
+            i += 1
+            j-=1
+        return "".join(s)
